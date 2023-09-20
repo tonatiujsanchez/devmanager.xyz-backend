0b2fac26623a2e87dfc840159446967b6602ca66
Tonatiuj Sánchez <tonsanzjimz@gmail.com>
2023-09-20 10:50:27 -0600

    Configuración de Socket.IO implementada

---
+++ controllers/socketIO.ts
@@ -0,0 +1,10 @@
+import { Server, Socket } from "socket.io"
+
+
+export const socketController = async(socket:Socket, io:Server) => {
+
+
+    console.log('====================> Hola desde socketController <====================');
+    
+
+}
--- models/server.ts
+++ models/server.ts
@@ -1,15 +1,23 @@
 import express, { Application } from "express"
 
+import socketIO from "socket.io"
+import { createServer, Server as HttpServer } from "http"
+
 import cors from 'cors'
 
 import { dbConnection } from '../config/db'
 import { projectsRoutes, usersRoutes, tasksRoutes, collaboratorsRoutes } from "../routes"
+
+import { socketController } from "../controllers/socketIO"
 
 
 class Server {
 
     private app: Application
     private port: string
+    private server: HttpServer
+    private io: socketIO.Server
+
     private paths = {
         users   : '/api/users',
         projects: '/api/projects',
@@ -20,6 +28,13 @@ class Server {
     constructor() {
         this.app = express()
         this.port =  process.env.PORT || '4000'
+        this.server = createServer(this.app)
+        this.io = new socketIO.Server( this.server, {
+            pingTimeout:60000,
+            cors:{
+                origin:process.env.FRONTEND_URL
+            }
+        })
 
         // db
         this.dbConnection()
@@ -29,10 +44,12 @@ class Server {
 
         // Routes
         this.routes()
+
+        // sockets
+        this.sockets()
     }
 
-
-
+    
     async dbConnection() {
         await dbConnection()
     }
@@ -58,10 +75,13 @@ class Server {
             res.send('Hola mundo!! 👾')
         })
     }
-
+    
+    sockets(){
+        this.io.on('connection', ( socket )=> socketController(socket, this.io));
+    }
 
     listen() {
-        this.app.listen( this.port, ()=> {
+        this.server.listen( this.port, ()=> {
             console.log(`Servidor corriendo en el puerto ${ this.port }`)
         })
     }
